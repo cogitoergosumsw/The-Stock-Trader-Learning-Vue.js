f1c48d9d6d3cae987f49ffdcdff31a7f1da00d30
Seng Wee <sengwee_new@hotmail.com>
2019-03-11 11:08:25 +0800

    adding Buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,31 +1,51 @@
 <template lang="html">
-  <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
-      <div class="panel-heading">
-        <h3 class="panel-title">NAME <small>(Price: PRICE)</small></h3>
-      </div>
-      <div class="panel-body">
-        <div class="pull-left">
-          <input
-            type="number"
-            name=""
-            placeholder="Quantity"
-            value=""
-            class="form-control"
-          />
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">{{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input
+                            type="number"
+                            placeholder="Quantity"
+                            class="form-control"
+                            v-model.number="quantity"
+
+                    />
+                </div>
+                <div class="pull-right">
+                    <button :disabled="quantity<=0 || !Number.isInteger(quantity)" @click="buyStock"
+                            class="btn btn-success">
+                        Buy
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="pull-right">
-          <button type="button" class="btn btn-success" name="button">
-            Buy
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {};
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+            }
+        }
+    };
 </script>
 
 <style lang="css" scoped></style>
